5fec36791456d06cab01b692936d8d5034ce417d
Josh Shavers <shaversj@gmail.com>
2024-10-01 08:52:57 -0500

    Add html structure

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -3,7 +3,32 @@ import NxWelcome from './nx-welcome';
 export function App() {
   return (
     <div>
-      <NxWelcome title="client" />
+      {/*<NxWelcome title="client" />*/}
+
+      <header>
+        <nav></nav>
+      </header>
+
+      <div>
+        <h1>More than just shorter links</h1>
+      </div>
+
+      <div>
+      {/*  Search Bar  */}
+      </div>
+
+      <h2>Advanced Statistics</h2>
+      <p>Track how your links are performing across the web with our advanced statistics dashboard.</p>
+
+      <div>
+      {/*  Brand Recognition */}
+      </div>
+
+      <div>
+        <h3>Boost your links today</h3>
+      </div>
+
+      <footer></footer>
     </div>
   );
 }
