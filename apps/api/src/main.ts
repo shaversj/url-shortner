9ef3e95b683bf4dd4e6f56f3cd0e9175cf2211e2
Josh Shavers <shaversj@gmail.com>
2024-10-08 08:06:21 -0500

    Remove testing dependencies + Add API logic + Client Form Logic

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,8 +5,12 @@
 
 import express from 'express';
 import * as path from 'path';
+import { nanoid } from 'nanoid'
+import cors from 'cors';
 
 const app = express();
+app.use(express.json());
+app.use(cors({ origin: 'http://localhost:4200' }));
 
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
@@ -14,8 +18,15 @@ app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to api!' });
 });
 
+app.post('/shorten', async (req, res) => {
+  const { originalUrl } = req.body;
+  const shortUrl = nanoid(8);
+  const newUrl = { originalURL: originalUrl, shorURL: shortUrl };
+  res.json(newUrl);
+});
+
 const port = process.env.PORT || 3333;
 const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
+  console.log(`Listening at http://localhost:${port}`);
 });
 server.on('error', console.error);
--- apps/client/src/app/components/ShortenLinkForm.tsx
+++ apps/client/src/app/components/ShortenLinkForm.tsx
@@ -1,11 +1,41 @@
+import {FieldErrors, SubmitHandler, useForm} from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
+
+type FormValues = {
+  url: string;
+};
+
+const FormSchema = z.object({
+  url: z.string().url(),
+});
+
 export default function ShortenLinkForm(){
+  const { register, handleSubmit, formState: { errors },} = useForm<FormValues>({resolver: zodResolver(FormSchema),});
+
+  const onSubmit: SubmitHandler<FormValues> = async (data, e: any) => {
+    try {
+      const response = await axios.post('http://localhost:3333/shorten', {
+        originalUrl: data.url,
+      });
+      console.log('Shortened URL:', response.data);
+    } catch (error) {
+      console.error('Error shortening URL:', error);
+    }
+  }
+
+  const onError = (errors: FieldErrors<FormValues>, e: any) => console.log("FORM ERRORS: ", errors, e);
+
   return (
-    <form
+    <form onSubmit={handleSubmit(onSubmit, onError)}
       className={"relative flex-col lg:flex-row px-6 py-6 gap-y-4 lg:gap-y-0 translate-y-1/2 bg-[url('/bg-shorten-mobile.svg')] lg:bg-[url('/bg-shorten-desktop.svg')] bg-right-top bg-darkPurple bg-no-repeat flex gap-x-6 lg:py-[3.25rem] lg:px-[4rem] rounded-lg"}>
       <input className={"rounded-lg h-16 pl-8 text-[1.25rem] leading-9 tracking-[0.009rem] font-medium grow"}
+             {...register("url")}
              type="text"
              placeholder="Shorten a link here..."/>
       <button
+        type={"submit"}
         className={"rounded-lg w-full bg-aqua h-16 lg:w-[10.5rem] text-[1.25rem] text-white leading-[1.875rem] font-bold"}>Shorten
         It!
       </button>
