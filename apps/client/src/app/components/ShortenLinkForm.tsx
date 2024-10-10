dbdacc687e6ccbb5c32acd9f800e759e60288ddf
Josh Shavers <shaversj@gmail.com>
2024-10-10 08:09:22 -0500

    Add prettier + Update formatting

--- apps/client/src/app/components/ShortenLinkForm.tsx
+++ apps/client/src/app/components/ShortenLinkForm.tsx
@@ -1,21 +1,25 @@
-import {FieldErrors, SubmitHandler, useForm} from "react-hook-form";
-import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import axios from "axios";
+import { FieldErrors, SubmitHandler, useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import axios from 'axios';
 
 type FormValues = {
   url: string;
 };
 
 const FormSchema = z.object({
-  url: z.string().url({message: "Please enter a valid URL"}),
+  url: z.string().url({ message: 'Please enter a valid URL' }),
 });
 
-export default function ShortenLinkForm({addUrl}: {addUrl: (url: any) => void}) {
-  const {register, handleSubmit, formState: {errors},} = useForm<FormValues>({resolver: zodResolver(FormSchema),});
+export default function ShortenLinkForm({ addUrl }: { addUrl: (url: any) => void }) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>({ resolver: zodResolver(FormSchema) });
 
   const onSubmit: SubmitHandler<FormValues> = async (data, e: any) => {
-    console.log("FORM DATA: ", data, e);
+    console.log('FORM DATA: ', data, e);
     try {
       const response = await axios.post('http://localhost:3333/shorten', {
         originalUrl: data.url,
@@ -25,34 +29,38 @@ export default function ShortenLinkForm({addUrl}: {addUrl: (url: any) => void}) 
     } catch (error) {
       console.error('Error shortening URL:', error);
     }
-  }
+  };
 
-  const onError = (errors: FieldErrors<FormValues>, e: any) => console.log("FORM ERRORS: ", errors, e);
+  const onError = (errors: FieldErrors<FormValues>, e: any) => console.log('FORM ERRORS: ', errors, e);
 
   return (
     <>
-      <form onSubmit={handleSubmit(onSubmit, onError)}
-            className={"relative flex-col md:flex-row px-6 py-6 gap-y-4 md:gap-y-0 translate-y-1/2 bg-[url('/bg-shorten-mobile.svg')] md:bg-[url('/bg-shorten-desktop.svg')] bg-right-top bg-darkPurple bg-no-repeat flex gap-x-6 md:py-[3.25rem] md:px-[4rem] rounded-lg"}>
+      <form
+        onSubmit={handleSubmit(onSubmit, onError)}
+        className={
+          "bg-darkPurple relative flex translate-y-1/2 flex-col gap-x-6 gap-y-4 rounded-lg bg-[url('/bg-shorten-mobile.svg')] bg-right-top bg-no-repeat px-6 py-6 md:flex-row md:gap-y-0 md:bg-[url('/bg-shorten-desktop.svg')] md:px-[4rem] md:py-[3.25rem]"
+        }
+      >
         <input
-          className={"rounded-lg h-16 pl-8 text-[1.25rem] leading-9 tracking-[0.009rem] font-medium grow aria-[invalid=true]:border-[0.188rem] aria-[invalid=true]:border-red aria-[invalid=true]:placeholder:text-red aria-[invalid=true]:placeholder:opacity-50 outline-none"}
-          {...register("url")}
+          className={
+            'aria-[invalid=true]:border-red aria-[invalid=true]:placeholder:text-red h-16 grow rounded-lg pl-8 text-[1.25rem] font-medium leading-9 tracking-[0.009rem] outline-none aria-[invalid=true]:border-[0.188rem] aria-[invalid=true]:placeholder:opacity-50'
+          }
+          {...register('url')}
           type="text"
           placeholder="Shorten a link here..."
-          aria-invalid={errors.url ? "true" : "false"}
+          aria-invalid={errors.url ? 'true' : 'false'}
         />
 
-        {errors.url && <span
-          className={"text-xs md:text-[1rem] tracking-[0.005rem] absolute translate-y-[4.25rem] md:translate-y-20 font-medium leading-[1.125rem] md:tracking-[0.007rem] text-red"}>{errors.url.message}</span>}
+        {errors.url && (
+          <span className={'text-red absolute translate-y-[4.25rem] text-xs font-medium leading-[1.125rem] tracking-[0.005rem] md:translate-y-20 md:text-[1rem] md:tracking-[0.007rem]'}>
+            {errors.url.message}
+          </span>
+        )}
 
-        <button
-          type={"submit"}
-          className={`rounded-lg w-full bg-aqua h-16 md:w-[10.5rem] text-[1.25rem] text-white leading-[1.875rem] font-bold ${errors.url ? "mt-[1.125rem] md:mt-0" : ""}`}>Shorten
-          It!
+        <button type={'submit'} className={`bg-aqua h-16 w-full rounded-lg text-[1.25rem] font-bold leading-[1.875rem] text-white md:w-[10.5rem] ${errors.url ? 'mt-[1.125rem] md:mt-0' : ''}`}>
+          Shorten It!
         </button>
-
       </form>
-
     </>
-
-  )
+  );
 }
--- apps/client/src/app/components/StatisticsHeader.tsx
+++ apps/client/src/app/components/StatisticsHeader.tsx
@@ -1,10 +1,10 @@
-export default function StatisticsHeader(){
+export default function StatisticsHeader() {
   return (
-    <div className={"w-[327px] lg:w-[33.75rem] mx-auto"}>
-      <h2 className={"lg:text-[2.5rem] text-[1.75rem] leading-[3rem] font-bold tracking-[-0.044rem] lg:tracking-[-0.063rem] text-darkGray text-center"}>Advanced
-        Statistics</h2>
-      <p className={"leading-6 tracking-[0.007rem] lg:text-[1.125rem] lg:leading-8 lg:tracking-[0.008rem] text-lightGray text-center pt-[1.125rem]"}>Track how
-        your links are performing across the web with our advanced statistics dashboard.</p>
+    <div className={'mx-auto w-[327px] lg:w-[33.75rem]'}>
+      <h2 className={'text-darkGray text-center text-[1.75rem] font-bold leading-[3rem] tracking-[-0.044rem] lg:text-[2.5rem] lg:tracking-[-0.063rem]'}>Advanced Statistics</h2>
+      <p className={'text-lightGray pt-[1.125rem] text-center leading-6 tracking-[0.007rem] lg:text-[1.125rem] lg:leading-8 lg:tracking-[0.008rem]'}>
+        Track how your links are performing across the web with our advanced statistics dashboard.
+      </p>
     </div>
-  )
+  );
 }
