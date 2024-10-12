aaf4a6278c3ad010133bf8169565a610c5b957a3
Josh Shavers <shaversj@gmail.com>
2024-10-12 15:07:15 -0500

    Save urls to localstorage

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -1,28 +1,39 @@
 import NxWelcome from './nx-welcome';
-import FeatureList from "./components/FeatureList";
-import StatisticsHeader from "./components/StatisticsHeader";
-import StatisticsSection from "./components/StatisticsSection";
-import HeroSection from "./components/HeroSection";
-import Header from "./components/Header";
-import ShortenLinkForm from "./components/ShortenLinkForm";
-import MainSection from "./components/MainSection";
-import Footer from "./components/Footer";
-import BoostSection from "./components/BoostSection";
-import {useState} from "react";
-import UrlCardList from "./UrlCardList";
-import UrlSection from "./components/UrlSection";
+import FeatureList from './components/FeatureList';
+import StatisticsHeader from './components/StatisticsHeader';
+import StatisticsSection from './components/StatisticsSection';
+import HeroSection from './components/HeroSection';
+import Header from './components/Header';
+import ShortenLinkForm from './components/ShortenLinkForm';
+import MainSection from './components/MainSection';
+import Footer from './components/Footer';
+import BoostSection from './components/BoostSection';
+import { useEffect, useState } from 'react';
+import UrlCardList from './UrlCardList';
+import UrlSection from './components/UrlSection';
 
 export type UrlObject = {
   originalUrl: string;
   shortUrl: string;
-}
+};
 
 export function App() {
   const [urls, setUrls] = useState<UrlObject[]>([]);
-  const addUrl = (url: UrlObject) => setUrls([...urls, url]);
+  const addUrl = (url: UrlObject) => {
+    const newUrls = [...urls, url];
+    setUrls(newUrls);
+    localStorage.setItem('urls', JSON.stringify(newUrls));
+  };
+
+  useEffect(() => {
+    const urls = localStorage.getItem('urls');
+    if (urls) {
+      setUrls(JSON.parse(urls));
+    }
+  }, []);
 
   return (
-    <div className={"font-poppins"}>
+    <div className={'font-poppins'}>
       {/*<NxWelcome title="client" />*/}
       <MainSection>
         <Header />
@@ -31,12 +42,12 @@ export function App() {
       </MainSection>
       {urls.length > 0 && (
         <UrlSection>
-          <UrlCardList urls={urls}/>
+          <UrlCardList urls={urls} />
         </UrlSection>
       )}
       <StatisticsSection urls={urls}>
-        <StatisticsHeader/>
-        <FeatureList/>
+        <StatisticsHeader />
+        <FeatureList />
       </StatisticsSection>
       <BoostSection />
       <Footer />
