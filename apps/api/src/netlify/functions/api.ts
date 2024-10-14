23a35c3470b6e5bc7f7e52fb2798bfc7a9dfa333
Josh Shavers <shaversj@gmail.com>
2024-10-14 08:09:07 -0500

    Add function

---
+++ apps/api/src/netlify/functions/api.ts
@@ -0,0 +1,38 @@
+import express from 'express';
+import * as path from 'path';
+import { nanoid } from 'nanoid';
+import cors from 'cors';
+import serverless from 'serverless-http';
+
+const app = express();
+const router = express.Router();
+
+app.use(express.json());
+app.use(cors());
+
+router.get('/hello', (req, res) => {
+  res.json({ message: 'Welcome to api!' });
+});
+
+app.use('/.netlify/functions/api', router);
+
+export const handler = serverless(app);
+
+// app.use('/assets', express.static(path.join(__dirname, 'assets')));
+//
+// app.get('/api', (req, res) => {
+//   res.send({ message: 'Welcome to api!' });
+// });
+
+/*app.post('/shorten', async (req, res) => {
+  const { originalUrl } = req.body;
+  const shortUrl = nanoid(8);
+  const newUrl = { originalURL: originalUrl, shortURL: shortUrl };
+  res.json(newUrl);
+});
+
+const port = process.env.PORT || 3333;
+const server = app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
+server.on('error', console.error);*/
