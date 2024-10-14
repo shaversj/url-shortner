9c2917b045970adf5fd80c812bf1fcc547de5d23
Josh Shavers <shaversj@gmail.com>
2024-10-14 08:47:44 -0500

    Update api + deploy config

--- apps/api/src/api.ts
+++ apps/api/src/api.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import * as path from 'path';
 import { nanoid } from 'nanoid';
 import cors from 'cors';
 import serverless from 'serverless-http';
@@ -9,29 +8,11 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/.netlify/functions/api', (req, res) => {
-  res.json({ message: 'Welcome to api!' });
-});
-
-// app.use('/.netlify/functions/api', router);
-
-export const handler = serverless(app);
-
-// app.use('/assets', express.static(path.join(__dirname, 'assets')));
-//
-// app.get('/api', (req, res) => {
-//   res.send({ message: 'Welcome to api!' });
-// });
-
-/*app.post('/shorten', async (req, res) => {
+app.post('/.netlify/functions/api/shorten', async (req, res) => {
   const { originalUrl } = req.body;
   const shortUrl = nanoid(8);
   const newUrl = { originalURL: originalUrl, shortURL: shortUrl };
   res.json(newUrl);
 });
 
-const port = process.env.PORT || 3333;
-const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`);
-});
-server.on('error', console.error);*/
+export const handler = serverless(app);
