dec8ce41d107939b763b727cc7d7b1cc6cc6e586
Josh Shavers <shaversj@gmail.com>
2024-10-20 11:50:16 -0500

    Update layout

--- apps/client/src/app/components/FooterWrapper.tsx
+++ apps/client/src/app/components/FooterWrapper.tsx
@@ -1,3 +1,3 @@
 export default function FooterWrapper({ children }: React.PropsWithChildren<{}>) {
-  return <footer className={'bg-darkBrown py-[4.5rem] lg:min-h-[20.625rem] lg:px-[10.313rem] lg:pb-0 lg:pt-[4.5rem]'}>{children}</footer>;
+  return <footer className={'bg-darkBrown py-[4.5rem] lg:px-[10.313rem]'}>{children}</footer>;
 }
