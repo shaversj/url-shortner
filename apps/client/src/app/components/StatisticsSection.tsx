f44a6f95fdd295bb2008c5ff43f19af774693a29
Josh Shavers <shaversj@gmail.com>
2024-10-20 12:22:21 -0500

    Update height

--- apps/client/src/app/components/StatisticsSection.tsx
+++ apps/client/src/app/components/StatisticsSection.tsx
@@ -7,6 +7,6 @@ type StatisticsSectionProps = {
 
 export default function StatisticsSection({ children, urls }: StatisticsSectionProps) {
   return (
-    <section className={`bg-lightBlue h-[1490px] px-6 pb-[5rem] pt-[10rem] lg:h-full lg:px-[10.313rem] lg:pb-[10.5rem] ${urls.length > 0 ? 'lg:pt-[120px]' : 'lg:pt-[12.75rem]'}`}>{children}</section>
+    <section className={`bg-lightBlue h-[1490px] px-6 pb-[5rem] pt-[10rem] lg:h-full lg:px-[10.313rem] lg:pb-[10.75rem] ${urls.length > 0 ? 'lg:pt-[120px]' : 'lg:pt-[12.75rem]'}`}>{children}</section>
   );
 }
